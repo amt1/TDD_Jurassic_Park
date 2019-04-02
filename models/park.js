47461b2d8db433d013c333b6574cd00f549e9038
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-04-03 00:15:40 +0100

    only number functions left to do

--- models/park.js
+++ models/park.js
@@ -4,14 +4,80 @@ const Park = function (name, ticketPrice, dinosaurs) {
   this.dinosaurs = dinosaurs;
 }
 
+// Done - Add a dinosaur to its collection of dinosaurs
+
 Park.prototype.add_dinosaur = function( dinosaur){
   this.dinosaurs.push(dinosaur);
 };
 
-// Done - Add a dinosaur to its collection of dinosaurs
-// - Remove a dinosaur from its collection of dinosaurs
-// - Find the dinosaur that attracts the most visitors
+// done - Remove a dinosaur from its collection of dinosaurs
+
+Park.prototype.remove_a_dinosaur = function( dinosaur){
+  let my_dino = null;
+  let found = this.dinosaurs.includes(dinosaur);
+   if (found) {
+    my_dino = this.dinosaurs.splice(this.dinosaurs.indexOf(dinosaur), 1);
+}
+return my_dino;
+};
+
+Park.prototype.compare_popularity = function(x,y) {
+// somehow this knows to get the two objects for comparing
+    const guestsX = x.guestsAttractedPerDay;
+    const guestsY = y.guestsAttractedPerDay;
+// this gets the values out of the objects
+    let comparison = 0;
+    if (guestsX < guestsY) {
+      comparison = 1;
+    } else if (guestsX > guestsY) {
+      comparison = -1;
+    }
+    return comparison;
+
+// sets a standard  positive or negative numerical difference
+// after comparing the 2 values
+// to indicate the direction for sorting them
+// sorting by numbers needs its own function
+// because otherwise they are sorted as strings
+};
+
+// done - Find the dinosaur that attracts the most visitors
+
+Park.prototype.most_popular_dino = function(){
+  this.dinosaurs.sort(this.compare_popularity);
+return this.dinosaurs[0];
+};
+
 // - Find all dinosaurs of a particular species
+// - Assuming this means locate them not fetch them:
+
+Park.prototype.locate_all = function(species_to_find){
+  var locations = [];
+  for (var dino of this.dinosaurs) {
+//    console.log(dino); // lists dinosaur objects
+    if (dino.species === species_to_find) {
+      locations.push(this.dinosaurs.indexOf(dino));
+    };
+  };
+return locations;
+};
+
+// - Remove all dinosaurs of a particular species
+// - Assuming this means remove them and return them:
+
+Park.prototype.remove_all = function(species_to_remove){
+  var counter = this.dinosaurs.length;
+  var corral = [];
+  while (counter--) {
+    if (this.dinosaurs[counter].species === species_to_remove) {
+      corral.push( this.dinosaurs.splice(counter,1)[0] );
+    };
+  };
+  return corral;
+};
+// note array returned is reversed due to loop counting backwards
+
+
 // - Calculate the total number of visitors per day
 // - Calculate the total number of visitors per year
 // - Calculate the total revenue from ticket sales for one year
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -9,6 +9,13 @@ describe('Park', function() {
   beforeEach(function () {
       park = new Park('Jurassic Park', 100, []);
       dinosaur1 = new Dinosaur("Triceratops", "Plants", 100);
+      dinosaur2 = new Dinosaur("Diplodocus", "Plants", 200);
+      dinosaur3 = new Dinosaur("Pterodactyl", "Fish", 150);
+      dinosaur4 = new Dinosaur("Velociraptor", "Everything", 10);
+      dinosaur5 = new Dinosaur("Triceratops", "Plants", 100);
+      dinosaur6 = new Dinosaur("Velociraptor", "Everything", 10);
+
+
   });
 
   it('should have a name', function () {
@@ -32,12 +39,57 @@ describe('Park', function() {
     const actual = park.dinosaurs;
     assert.deepStrictEqual(actual, [dinosaur1]);
   });
-  // it('should be able to remove a dinosaur from its collection');
-  //
-  // it('should be able to find the dinosaur that attracts the most visitors');
-  //
-  // it('should be able to find all dinosaurs of a particular species');
-  //
-  // it('should be able to remove all dinosaurs of a particular species');
+
+  it('should be able to remove a dinosaur from its collection', function () {
+    park.add_dinosaur(dinosaur1)
+    park.add_dinosaur(dinosaur2)
+    park.add_dinosaur(dinosaur3)
+    park.add_dinosaur(dinosaur4)
+    park.add_dinosaur(dinosaur5)
+
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [dinosaur1,dinosaur2,dinosaur3,dinosaur4,dinosaur5]);
+    park.remove_a_dinosaur(dinosaur4)
+    const actual2 = park.dinosaurs;
+    assert.deepStrictEqual(actual2, [dinosaur1,dinosaur2,dinosaur3,dinosaur5]);
+  });
+
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    park.add_dinosaur(dinosaur1)
+    park.add_dinosaur(dinosaur2)
+    park.add_dinosaur(dinosaur3)
+    park.add_dinosaur(dinosaur4)
+    park.add_dinosaur(dinosaur5)
+
+    const actual = park.most_popular_dino();
+    assert.strictEqual(actual, dinosaur2);
+  });
+
+  it('should be able to find all dinosaurs of a particular species', function () {
+    park.add_dinosaur(dinosaur1)
+    park.add_dinosaur(dinosaur2)
+    park.add_dinosaur(dinosaur3)
+    park.add_dinosaur(dinosaur4)
+    park.add_dinosaur(dinosaur5)
+    park.add_dinosaur(dinosaur6)
+
+    const actual = park.locate_all("Velociraptor");
+    assert.deepStrictEqual(actual, [3,5]);
+  });
+
+  it('should be able to remove all dinosaurs of a particular species', function () {
+    park.add_dinosaur(dinosaur1)
+    park.add_dinosaur(dinosaur2)
+    park.add_dinosaur(dinosaur3)
+    park.add_dinosaur(dinosaur4)
+    park.add_dinosaur(dinosaur5)
+    park.add_dinosaur(dinosaur6)
+    dinosaur1.species="Velociraptor";
+    const dinoCorral = park.remove_all("Velociraptor");
+    const dinosRemaining = park.dinosaurs;
+    assert.deepStrictEqual(dinoCorral, [dinosaur6,dinosaur4, dinosaur1]);
+    assert.deepStrictEqual(dinosRemaining, [dinosaur2,dinosaur3,dinosaur5]);
+
+  });
 
 });
